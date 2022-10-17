5a668ae8af10ffd81b4ef21d8deb304f55878de1
Michael Wolfe <mw07f3@gmail.com>
2022-10-16 21:52:12 -0400

    Add tests

---
+++ Tests/com/wolfe/textanalyzerfx/TextAnalyzerFXTest.java
@@ -0,0 +1,39 @@
+package com.wolfe.textanalyzerfx;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextAnalyzerFXTest {
+
+    // create HashMap to test sortWords method of TextAnalyzerFX.java
+    public static HashMap<String, Integer> testHashMap() {
+        HashMap<String, Integer> testMap = new HashMap<>();
+        testMap.put("xyz", 2);
+        testMap.put("qrs", 1);
+        testMap.put("abc", 3);
+        return testMap;
+    }
+
+    // create comparison HashMap to verify output of sortWords method of TextAnalyzerFX.java
+    public static HashMap<String, Integer> baseHashMap() {
+        HashMap<String, Integer> baseMap = new HashMap<>();
+        baseMap.put("abc", 3);
+        baseMap.put("xyz", 2);
+        baseMap.put("qrs", 1);
+        return baseMap;
+    }
+
+    // validate output of sortWords method of TextAnalyzerFX.java
+    @Test
+    void sortWords() {
+
+        assertEquals(baseHashMap(), TextAnalyzerFX.sortWords(testHashMap()));
+    }
+
+
+}
--- Tests/com/wolfe/textanalyzerfx/WordsTest.java
+++ Tests/com/wolfe/textanalyzerfx/WordsTest.java
@@ -5,26 +5,32 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordsTest {
+
+    // create object to be used for testing
     private final Words testWord = new Words("test", 99);
 
+    // validate getWord method
     @Test
     void getWord() {
 
         assertEquals("test", testWord.getWord());
     }
 
+    // validate getCount method
     @Test
     void getCount() {
 
         assertEquals(99, testWord.getCount());
     }
 
+    // validate setCount method
     @Test
     void changeCount() {
         testWord.setCount(88);
         assertEquals(88, testWord.getCount());
     }
 
+    // validate setWord method
     @Test
     void changeWord() {
         testWord.setWord("test1");
